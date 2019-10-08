7fba0b39550e80662d93ab20d96ab92a5c368e8d
Thomas Hansen <thomas@gaiasoul.com>
2019-10-08 18:30:51 +0300

    Update MsSQLTestsRealDB.cs

--- magic.lambda.mssql.tests/MsSQLTestsRealDB.cs
+++ magic.lambda.mssql.tests/MsSQLTestsRealDB.cs
@@ -4,6 +4,26 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
+
+/*
+ * These unit tests requires the following database table to exist somewhere.
+ * And it also requires the "_connection" field of the class to be pointing to
+ * that database containing the "Demo" table.
+ * 
+ * Notice! To run these tests, uncomment the first line in the file, to make
+ * sure the compiler includes the C# code while compiling your tests suite.
+
+
+
+CREATE TABLE [dbo].[Demo] (
+  [Id] INT IDENTITY (1, 1) NOT NULL,
+  [text] NCHAR (100) NULL,
+  PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+ */
+
 using System.Linq;
 using Xunit;
 using magic.node.extensions;
@@ -13,22 +33,24 @@ namespace magic.lambda.mssql.tests
 {
     public class MsSQLTestsRealDB
     {
+        // TODO: Modify this connection string if you intend to run these tests.
+        const string _connection = "Server=localhost;Database=foo;Trusted_Connection=True;";
 #if RUN_REAL
 
         [Fact]
         public void SelectFromDemo_01()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.read
-      table:Demo");
+      table:Demo", _connection));
         }
 
         [Fact]
         public void InsertAndSelectFromDemo_01()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.create
       table:Demo
       values
@@ -38,7 +60,7 @@ mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
       where
          and
             text:Howdy world
-");
+", _connection));
             var result = new Expression("../*/1/*/*/text").Evaluate(lambda);
             Assert.NotEmpty(result);
         }
@@ -46,8 +68,8 @@ mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
         [Fact]
         public void InsertAndSelectLimitFromDemo_01()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.create
       table:Demo
       values
@@ -58,7 +80,7 @@ mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
          and
             text:Howdy world
       limit:5
-");
+", _connection));
             var result = new Expression("../*/1/*/*/text").Evaluate(lambda);
             Assert.True(result.Count() <= 5);
             Assert.NotEmpty(result);
@@ -67,8 +89,8 @@ mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
         [Fact]
         public void InsertAndUpdateExpressionClause_01()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.create
       table:Demo
       values
@@ -86,7 +108,7 @@ mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
          and
             text:Jo dudes!
       limit:5
-");
+", _connection));
             var result = new Expression("../*/1/*/*/text").Evaluate(lambda);
             Assert.True(result.Count() <= 5);
         }
@@ -94,45 +116,45 @@ mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
         [Fact]
         public void InsertTransactionRollbackExplicitly()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.transaction.create
       mssql.create
          table:Demo
          values
             text:Non existing
       mssql.transaction.rollback
-   mssql.scalar:""select count(*) from Demo where text = 'Non existing'""");
+   mssql.scalar:""select count(*) from Demo where text = 'Non existing'""", _connection));
             Assert.Equal(0, lambda.Children.First().Children.Skip(1).First().Value);
         }
 
         [Fact]
         public void InsertTransactionRollbackImplicitly()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.transaction.create
       mssql.create
          table:Demo
          values
             text:Non existing
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
-   mssql.scalar:""select count(*) from Demo where text = 'Non existing'""");
+mssql.connect:""{0}""
+   mssql.scalar:""select count(*) from Demo where text = 'Non existing'""", _connection));
             Assert.Equal(0, lambda.Children.Skip(1).First().Children.First().Value);
         }
 
         [Fact]
         public void InsertTransactionCommitExplicitly()
         {
-            var lambda = Common.Evaluate(@"
-mssql.connect:""Server=localhost;Database=foo;Trusted_Connection=True;""
+            var lambda = Common.Evaluate(string.Format(@"
+mssql.connect:""{0}""
    mssql.transaction.create
       mssql.create
          table:Demo
          values
             text:Non existing - yet does
       mssql.transaction.commit
-   mssql.scalar:""select count(*) from Demo where text = 'Non existing - yet does'""");
+   mssql.scalar:""select count(*) from Demo where text = 'Non existing - yet does'""", _connection));
             Assert.True(lambda.Children.First().Children.Skip(1).First().Get<int>() > 0);
         }
 #endif
