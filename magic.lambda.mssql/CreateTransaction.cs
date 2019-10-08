032857e9b51e428c980ba5d1b3b1bb5e05f01d23
Thomas Hansen <thomas@gaiasoul.com>
2019-10-08 10:46:17 +0300

    Update CreateTransaction.cs

--- magic.lambda.mssql/CreateTransaction.cs
+++ magic.lambda.mssql/CreateTransaction.cs
@@ -3,6 +3,7 @@
  * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
  */
 
+using System.Data.Common;
 using magic.node;
 using magic.data.common;
 using magic.signals.contracts;
@@ -22,7 +23,7 @@ namespace magic.lambda.mysql
         /// <param name="input">Root node for invocation.</param>
 		public void Signal(ISignaler signaler, Node input)
 		{
-            signaler.Scope("mssql.transaction", new Transaction(signaler, "mssql.transaction"), () =>
+            signaler.Scope("mssql.transaction", new Transaction(signaler, signaler.Peek<DbConnection>("mysql.connect")), () =>
             {
                 signaler.Signal("eval", input);
             });
