aa280b1095ac38b428f3a9b194253855933c4c6c
Thomas Hansen <thomas@gaiasoul.com>
2019-10-11 15:11:13 +0300

    Updating copyright notice

--- magic.lambda.mssql.tests/MsSQLTestsRealDB.cs
+++ magic.lambda.mssql.tests/MsSQLTestsRealDB.cs
@@ -1,7 +1,7 @@
 //#define RUN_REAL
 /*
- * Magic, Copyright(c) Thomas Hansen 2019 - thomas@gaiasoul.com
- * Licensed as Affero GPL unless an explicitly proprietary license has been obtained.
+ * Magic, Copyright(c) Thomas Hansen 2019, thomas@gaiasoul.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
  */
 
 
