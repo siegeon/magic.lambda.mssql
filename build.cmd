e18f61cb7d9766d68581cb9a9975f1e78e864ff7
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 16:26:08 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
 cd %~dp0
-dotnet build magic.lambda.mssql/magic.lambda.mssql.csproj --configuration Release
+dotnet build magic.lambda.mssql/magic.lambda.mssql.csproj --configuration Release --source https://api.nuget.org/v3/index.json
