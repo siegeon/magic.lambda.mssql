cc3cd6f69d82ad5f6433ab966b95172273e8b77e
Thomas Hansen <thomas@gaiasoul.com>
2020-01-16 17:58:17 +0200

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -3,6 +3,5 @@ set version=%1
 set key=%2
 
 cd %~dp0
-
 dotnet build magic.lambda.mssql/magic.lambda.mssql.csproj --configuration Release --source https://api.nuget.org/v3/index.json
 dotnet nuget push magic.lambda.mssql/bin/Release/magic.lambda.mssql.%version%.nupkg -k %key% -s https://api.nuget.org/v3/index.json
