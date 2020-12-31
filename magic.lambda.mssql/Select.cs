a7999c2334ace95580d5df35b38b137d8d2f5f6a
Thomas Hansen <thomas@gaiasoul.com>
2020-12-31 15:02:00 +0200

    Removing multiple result set node before execution to avoid having it become a part of the parameter collection

--- magic.lambda.mssql/Select.cs
+++ magic.lambda.mssql/Select.cs
@@ -27,8 +27,9 @@ namespace magic.lambda.mssql
         /// <param name="input">Arguments to your slot.</param>
         public void Signal(ISignaler signaler, Node input)
         {
-            var multipleResultSets = input.Children
-                .FirstOrDefault(x => x.Name == "multiple-result-sets")?.GetEx<bool>() ?? false;
+            var mrsNode = input.Children.FirstOrDefault(x => x.Name == "multiple-result-sets");
+            var multipleResultSets = mrsNode?.GetEx<bool>() ?? false;
+            mrsNode?.UnTie();
             Executor.Execute(
                 input,
                 signaler.Peek<SqlConnectionWrapper>("mssql.connect").Connection,
@@ -74,8 +75,9 @@ namespace magic.lambda.mssql
         /// <returns>An awaitable task.</returns>
         public async Task SignalAsync(ISignaler signaler, Node input)
         {
-            var multipleResultSets = input.Children
-                .FirstOrDefault(x => x.Name == "multiple-result-sets")?.GetEx<bool>() ?? false;
+            var mrsNode = input.Children.FirstOrDefault(x => x.Name == "multiple-result-sets");
+            var multipleResultSets = mrsNode?.GetEx<bool>() ?? false;
+            mrsNode?.UnTie();
             await Executor.ExecuteAsync(
                 input,
                 signaler.Peek<SqlConnectionWrapper>("mssql.connect").Connection,
